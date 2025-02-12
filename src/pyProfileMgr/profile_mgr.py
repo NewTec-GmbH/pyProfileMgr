de7a2fe997351a005d4f4e3aba03ee47a2293d77
Stefan Vogel <stefan.vogel@newtec.de>
2025-02-12 14:57:47 +0100

    Fix linter directive

--- src/pyProfileMgr/profile_mgr.py
+++ src/pyProfileMgr/profile_mgr.py
@@ -103,7 +103,7 @@ class ProfileMgr:
 
         self._profiles_storage_path = self.prepare_profiles_folder()
 
-    # pylint: R0912, R0913, R0917
+    # pylint: disable=R0912, R0913, R0917
 
     def add(self,
             profile_name: str,
